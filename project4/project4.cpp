301391d7f61bbb9e50123b0f4e82c1bbb2d04ed8
Paige Lorson <43354596+lorsonp@users.noreply.github.com>
2019-05-13 09:46:02 -0700

    run

--- project4/project4.cpp
+++ project4/project4.cpp
@@ -2,15 +2,98 @@
 #include <stdio.h>
 #include <math.h>
 #include <iostream>
+#include "simd.p5.h"
 
-// #define NUMT	          1
+
+#ifndef          1
 #define ARRAYSIZE       1000000	// you decide
-// #define NUMTRIES        100	// you decide
+#endif
 
-float A[ARRAYSIZE];
-float B[ARRAYSIZE];
-float C[ARRAYSIZE];
+float *A = new float [ARRAYSIZE]
+float *B = new float [ARRAYSIZE]
+float *C = new float [ARRAYSIZE]
+
 using namespace std;
+
+
+void
+SimdMul( float *a, float *b,   float *c,   int len )
+{
+	int limit = ( len/SSE_WIDTH ) * SSE_WIDTH;
+	__asm
+	(
+		".att_syntax\n\t"
+		"movq    -24(%rbp), %r8\n\t"		// a
+		"movq    -32(%rbp), %rcx\n\t"		// b
+		"movq    -40(%rbp), %rdx\n\t"		// c
+	);
+
+	for( int i = 0; i < limit; i += SSE_WIDTH )
+	{
+		__asm
+		(
+			".att_syntax\n\t"
+			"movups	(%r8), %xmm0\n\t"	// load the first sse register
+			"movups	(%rcx), %xmm1\n\t"	// load the second sse register
+			"mulps	%xmm1, %xmm0\n\t"	// do the multiply
+			"movups	%xmm0, (%rdx)\n\t"	// store the result
+			"addq $16, %r8\n\t"
+			"addq $16, %rcx\n\t"
+			"addq $16, %rdx\n\t"
+		);
+	}
+
+	for( int i = limit; i < len; i++ )
+	{
+		c[i] = a[i] * b[i];
+	}
+}
+
+float
+SimdMulSum( float *a, float *b, int len )
+{
+	float sum[4] = { 0., 0., 0., 0. };
+	int limit = ( len/SSE_WIDTH ) * SSE_WIDTH;
+
+	__asm
+	(
+		".att_syntax\n\t"
+		"movq    -40(%rbp), %r8\n\t"		// a
+		"movq    -48(%rbp), %rcx\n\t"		// b
+		"leaq    -32(%rbp), %rdx\n\t"		// &sum[0]
+		"movups	 (%rdx), %xmm2\n\t"		// 4 copies of 0. in xmm2
+	);
+
+	for( int i = 0; i < limit; i += SSE_WIDTH )
+	{
+		__asm
+		(
+			".att_syntax\n\t"
+			"movups	(%r8), %xmm0\n\t"	// load the first sse register
+			"movups	(%rcx), %xmm1\n\t"	// load the second sse register
+			"mulps	%xmm1, %xmm0\n\t"	// do the multiply
+			"addps	%xmm0, %xmm2\n\t"	// do the add
+			"addq $16, %r8\n\t"
+			"addq $16, %rcx\n\t"
+		);
+	}
+}
+
+float
+NonSimdMulSum( float *a, float *b, int len )
+{
+	float sum[4] = { 0., 0., 0., 0. };
+	int limit = ( len/SSE_WIDTH ) * SSE_WIDTH;
+
+	for( int i = 0; i < len; i++ )
+	{
+		sum[0] += a[i] * b[i];
+	}
+
+	return sum[0];
+}
+
+
 
 int
 main( )
@@ -19,29 +102,89 @@ main( )
         fprintf( stderr, "OpenMP is not supported here -- sorry.\n" );
         return 1;
 #endif
+FILE *f1;
+FILE *f2;
+FILE *f3;
+FILE *f4;
+f1 = fopen("project4.simd.Mult.txt","a");
+f2 = fopen("project4.simd.sumMult.txt","a");
+f3 = fopen("project4.nonsimd.Mult.txt","a");
+f4 = fopen("project4.nonsimd.sumMult.txt","a");
 
-        // omp_set_num_threads( NUMT );
-        // fprintf( stderr, "Using %d threads\n", NUMT );
 
-        double maxMegaMults = 0.;
+        double minMilliSecs = 0.;
 
         for( int t = 0; t < NUMTRIES; t++ )
         {
                 double time0 = omp_get_wtime( );
 
-                // #pragma omp parallel for
                 for( int i = 0; i < ARRAYSIZE; i++ )
                 {
                         C[i] = A[i] * B[i];
+
                 }
 
                 double time1 = omp_get_wtime( );
-                double megaMults = (double)ARRAYSIZE/(time1-time0)/1000000.;
-                if( megaMults > maxMegaMults )
-                        maxMegaMults = megaMults;
+                double milliSecs = (time1-time0)*1000.;
+                if( milliSecs > minMilliSecs )
+                        minMilliSecs = milliSecs;
         }
-        cout << "it ran...\n";
-        printf( "Peak Performance = %8.2lf MegaMults/Sec\n", maxMegaMults );
+        printf( "Peak Performance = %8.2lf milliSecs\n", minMilliSecs );
+        fprintf(f3,"%f \n", minMilliSecs);
+
+        double minMilliSecs = 0.;
+
+        for( int t = 0; t < NUMTRIES; t++ )
+        {
+                double time0 = omp_get_wtime( );
+
+                for( int i = 0; i < ARRAYSIZE; i++ )
+                {
+                        C[i] = A[i] * B[i];
+
+                }
+
+                NonSimdMulSum( float *A, float *B, int ARRAYSIZE )
+
+                double time1 = omp_get_wtime( );
+                double milliSecs = (time1-time0)*1000.;
+                if( milliSecs > minMilliSecs )
+                        minMilliSecs = milliSecs;
+        }
+        printf( "Peak Performance = %8.2lf milliSecs\n", minMilliSecs );
+        fprintf(f4,"%f \n", minMilliSecs);
+
+        double minMilliSecs = 0.;
+
+        for( int t = 0; t < NUMTRIES; t++ )
+        {
+                double time0 = omp_get_wtime( );
+
+                SimdMul( float *A, float *B,   float *C,   int ARRAYSIZE )
+
+                double time1 = omp_get_wtime( );
+                double milliSecs = (time1-time0)*1000.;
+                if( milliSecs > minMilliSecs )
+                        minMilliSecs = milliSecs;
+        }
+        printf( "Peak Performance = %8.2lf milliSecs\n", minMilliSecs );
+        fprintf(f1,"%f \n", minMilliSecs);
+
+        double minMilliSecs = 0.;
+
+        for( int t = 0; t < NUMTRIES; t++ )
+        {
+                double time0 = omp_get_wtime( );
+
+                SimdMulSum( float *B, float *B, int ARRAYSIZE )
+
+                double time1 = omp_get_wtime( );
+                double milliSecs = (time1-time0)*1000.;
+                if( milliSecs > minMilliSecs )
+                        minMilliSecs = milliSecs;
+        }
+        printf( "Peak Performance = %8.2lf milliSecs\n", minMilliSecs );
+        fprintf(f1,"%f \n", minMilliSecs);
 
         return 0;
 }
