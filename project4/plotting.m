f8f57f075264781a30c3c7fb696dc1ec3e96f209
Paige Lorson <43354596+lorsonp@users.noreply.github.com>
2019-05-17 10:49:34 -0700

    p5

--- project4/plotting.m
+++ project4/plotting.m
@@ -15,9 +15,24 @@ data4 = importdata('project4.nonsimd.sumMult.txt',' ');
 ArraySize = [1000,2500,5000,7500,10000,25000,50000,75000,...
              100000,250000,500000,750000,1000000,2500000,...
              5000000,7500000,10000000];
-S_sumMult = data4./data2;
-S_Mult = data3./data1;
-table = [ArraySize',ArraySize'./[data1,data2,data3,data4]/1000];
+         
+% ArraySize = [ArraySize,ArraySize,ArraySize,ArraySize]; 
+data1 =  (data1(1:17) + data1(18:34) + data1(35:51)...
+       + data1(52:68) + data1(69:85) + data1(86:end))...
+       / 6;
+data2 =  (data2(1:17) + data2(18:34) + data2(35:51)...
+       + data2(52:68) + data2(69:85) + data2(86:end))...
+       / 6;
+
+data3 =  (data3(1:17) + data3(18:34) + data3(35:51)...
+       + data3(52:68) + data3(69:85) + data3(86:end))...
+       / 6;
+data4 =  (data4(1:17) + data4(18:34) + data4(35:51)...
+       + data4(52:68) + data4(69:85) + data4(86:end))...
+       / 6;
+S_sumMult = data2./data4;
+S_Mult = data1./data3;
+table = [data1,data2,data3,data4];
 latex(vpa(sym(table),4))
 
 fig1 = figure(1); 
@@ -34,15 +49,13 @@ legend('Sum-Mult','Mult','Location','south','Orientation','horizontal','FontSize
 title('Results for Simulation','interpreter','latex','FontSize', 20)
 set(gcf,'Color','w')
 
-
-
 fig2 = figure(2); 
 set(fig2,'units','normalized','outerposition',[0 0 0.75 1]);
-semilogx(ArraySize,ArraySize'./data1/1000,'LineStyle','-','LineWidth',3)
+semilogx(ArraySize,data1,'LineStyle','-','LineWidth',3)
 hold on
-semilogx(ArraySize,ArraySize'./data2/1000,'LineStyle','-','LineWidth',3)
-semilogx(ArraySize,ArraySize'./data3/1000,'LineStyle','-','LineWidth',3)
-semilogx(ArraySize,ArraySize'./data4/1000,'LineStyle','-','LineWidth',3)
+semilogx(ArraySize,data2,'LineStyle','-','LineWidth',3)
+semilogx(ArraySize,data3,'LineStyle','-','LineWidth',3)
+semilogx(ArraySize,data4,'LineStyle','-','LineWidth',3)
 
 set(gca,'LineWidth',0.5,'FontSize',12,'FontName','Times New Roman') 
 xlabel('Array Size','FontSize',18)
@@ -51,5 +64,3 @@ ylabel('Performance','FontSize',18)
 legend('simd.Mult','simd.sumMult','nonsimd.Mult','nonsimd.sumMult','Location','south','Orientation','horizontal','FontSize',14); 
 title('Results for Simulation','interpreter','latex','FontSize', 20)
 set(gcf,'Color','w')
-
-
